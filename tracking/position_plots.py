afde23e3f2d9dbd6a322f3829143a87be923593f
maelstrom <cephalopodmaster@protonmail.com>
2022-07-27 14:12:59 -0400

    Plot trajectories and heatmap for position

---
+++ tracking/position_plots.py
@@ -0,0 +1,72 @@
+################################################################################
+################################################################################
+
+
+import argparse
+import cv2 as cv
+import h5py
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import seaborn as sns
+
+
+n = 1 
+
+with open('../paths.json','r') as f:
+    paths = json.load(f)
+    codepath = paths['codepath']
+    datapath = os.path.dirname(str(paths['datapath']+
+                               '\\trajectories\\clustering\\'))+'\\'
+    videopath = os.path.dirname(str(paths['datapath']+
+                                '\\videos\\clustering\\'))+'\\'
+
+
+if __name__=='__main__':
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-f', '--file', required=True, nargs='+',
+                    help='Data file name')
+    ap.add_argument('-v', '--video', nargs='+',
+                    help='Video file name')
+
+    args = vars(ap.parse_args())
+        
+
+    cols = ['frame', 'orientation', 'head_x', 'head_y',
+            'thorax_x', 'thorax_y', 'abdomen_x', 'abdomen_y']
+
+    distances = pd.DataFrame()
+    ct = 0
+    
+    for fname in args['file']:
+        dfile = h5py.File('{}{}.hdf5'.format(datapath,fname), 'r')
+        #vfile = cv.VideoCapture('{}{}'.format(videopath, args['video']))
+        dframe = pd.DataFrame(columns=cols)
+        
+        for key in dfile.keys():
+            dset = dfile[key][:]
+            t, _, _ = np.shape(dset)
+            dset = dset.reshape([t,8])
+            df = pd.DataFrame(dset, columns=cols)
+            df['ant_number'] = ct
+
+            sns.scatterplot(x='thorax_x',y='thorax_y', data=df, alpha=0.1)
+            plt.savefig('{}_trajectory_{}.png'.format(fname,ct))
+            plt.close()
+
+            dframe = dframe.append(df, ignore_index=True)
+            ct += 1
+
+        sns.histplot(x='thorax_x', y='thorax_y', data=dframe, bins=(50,40), cbar=True)
+        plt.savefig('{}_histogram_alltime.png'.format(fname))
+        plt.close()
+
+    sns.histplot(x='thorax_x', y='thorax_y', data=dframe, bins=(50,40), cbar=True)
+    plt.savefig('n{}_histogram_alltime.png'.format(n))
+    plt.close()
+
+
+## EOF
