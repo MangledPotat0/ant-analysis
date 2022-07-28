058217d94f1257ddcf9dff38a716b48530621746
maelstrom <cephalopodmaster@protonmail.com>
2022-07-28 16:37:54 -0400

    Implemented simple speed distribution plot

---
+++ tracking/velocity_plots.py
@@ -0,0 +1,90 @@
+################################################################################
+################################################################################
+
+
+import argparse
+import cv2 as cv
+import h5py
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+from scipy import signal as sig
+import seaborn as sns
+
+
+n = 1
+
+with open('../paths.json','r') as f:
+    paths = json.load(f)
+    codepath = paths['codepath']
+    datapath = os.path.dirname(str(paths['datapath']+
+                               '\\trajectories\\clustering\\'))+'\\'
+    videopath = os.path.dirname(str(paths['datapath']+
+                                '\\videos\\clustering\\'))+'\\'
+
+
+def compute_speed(velocity):
+    hx, hy, tx, ty, ax, ay = np.transpose(velocity)
+    head = np.sqrt(hx**2 + hy**2)
+    thorax = np.sqrt(tx**2 + ty**2)
+    abdomen = np.sqrt(ax**2 + ay**2)
+
+    fullbody = np.transpose([head, thorax, abdomen])
+    mask = ~np.isnan(fullbody)
+    speeds = np.sum(fullbody, axis=1, where=mask)
+
+    return speeds
+
+if __name__=='__main__':
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-f', '--file', required=True, nargs='+',
+                    help='Data file name')
+    ap.add_argument('-v', '--video', nargs='+',
+                    help='Video file name')
+
+    args = vars(ap.parse_args())
+        
+
+    cols = ['frame', 'orientation', 'head_x', 'head_y',
+            'thorax_x', 'thorax_y', 'abdomen_x', 'abdomen_y']
+
+    ct = 0
+    
+    for fname in args['file']:
+        dfile = h5py.File('{}{}.hdf5'.format(datapath,fname), 'r')
+        #vfile = cv.VideoCapture('{}{}'.format(videopath, args['video']))
+        dframe = pd.DataFrame(columns=cols)
+        dspeed = pd.DataFrame()
+        
+        for key in dfile.keys():
+            dset = dfile[key][:]
+            t, _, _ = np.shape(dset)
+            dset = dset.reshape([t,8])
+            dset[1:,2:] = dset[1:,2:] - dset[:-1,2:]
+            dset = dset[1:]
+            df = pd.DataFrame(dset, columns=cols)
+            df['ant_number'] = ct
+            ds = pd.DataFrame(compute_speed(dset[:,2:]),
+                              columns=['speed (px/mm)'])
+
+            dspeed = dspeed.append(ds)
+            dframe = dframe.append(df, ignore_index=True)
+            ct += 1
+
+        print(dspeed.head())
+        g = sns.histplot(data=dspeed)
+        g.set_xlim(0,80)
+        g.set_ylim(0,7000)
+        plt.savefig('asdf.png')
+        plt.close()
+
+    sns.histplot(x='thorax_x', y='thorax_y', data=dframe, bins=(50,40), cbar=True)
+    plt.savefig('n{}_histogram_alltime.png'.format(n))
+    plt.close()
+
+
+
+## EOF
