3dc93e0dce81086034dcb58c2279911130207052
maelstrom <cephalopodmaster@protonmail.com>
2022-08-03 14:00:30 -0400

    Almost done, need to set up visualization and stuff

--- tracking/coverage.py
+++ tracking/coverage.py
@@ -38,12 +38,40 @@ def coverage(img_shape, array_shape, head, radius):
 
 if __name__ == '__main__':
 
+    radius = 5
+
     ap = argparse.ArgumentParser()
-    ap.add_argument('-f', '--file', required=True, nargs='+',
+    ap.add_argument('-f', '--file', required=True,
                     help='Data file name')
-    ap.add_argument('-v', '--video', nargs='+',
+    ap.add_argument('-v', '--video',
                     help='Video file name')
 
     args = vars(ap.parse_args())
+
+    dfile = hdf5.File('{}{}.hdf5'.format(datapath, args['file']), 'r')
+    vfile = cv.VideoCapture('{}{}.mp4'.format(vidpath, args['video']))
+
+    ret, frame = vfile.read()
+
+    ct = 0
+    while ret:
+        img_shape = np.shape(frame)[:1]
+        array_shape = img_shape/10
+        covered = np.full(array_shape, False)
+        for key in dfile.keys():
+            trajectory = dset[key][:]
+            idx = np.where(trajectory[:,0,0] == ct)
+            positions = trajectory[idx]
+            head = positions[1]
+            overwrite = coverage(img_shape, array_shape, head, radius)
+            covered += overwrite
+            
+        ret, frame = vfile.read()
+        ct += 1
+
+            
+            
+
         
 
+# EOF
