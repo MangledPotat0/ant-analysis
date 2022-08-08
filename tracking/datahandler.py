96c53ce91103c73d3da7a14ef7fbcfe78402f94b
maelstrom <cephalopodmaster@protonmail.com>
2022-08-08 17:01:23 -0400

    Basic functionalities added

---
+++ tracking/datahandler.py
@@ -0,0 +1,42 @@
+################################################################################
+################################################################################
+
+
+import numpy as np
+import pandas as pd
+
+
+# Data in the ant trajectory format
+# It should be a pandas dataframe with (t * n) shape where t = frame count.
+
+class trajectoryData:
+
+    def __init__(dframe):
+        self.trajectory_ = dframe
+        self.firstderivative_ = pd.DataFrame()
+
+    def trajectory(self):
+        return self.trajectory_
+
+    def set_trajectory(self, newdata):
+        self.trajectory_ = newdata
+
+    def firstderivative(self):
+        shape = np.shape(self.firstderivative_)
+        if shape == (0, 0):
+            self.firstderivative_ = self.derivative(data)
+        return self.firstderivative_
+
+    def derivative(self, dframe, order=1):
+
+        if order == 0:
+            print('Zeroth order derivative detected')
+
+        else:
+            while order > 0:
+                derivative = dframe.loc(1:,1:)
+                derivative = dframe.loc(1:,1:) - dframe.loc(:-2,1:)
+                dframe = derivative
+
+        return dframe
+               
