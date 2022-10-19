90f5a632158d8fecc4d5666e14f650357c926cbb
maelstrom <cephalopodmaster@protonmail.com>
2022-10-18 21:49:56 -0400

    Fixed minor issues with data types and variable names

--- common/local_montage.py
+++ common/local_montage.py
@@ -1,11 +1,12 @@
 import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
-def montage_generator(expid, trajectory, rawvid):
+def montage_generator(expid, trajectory, rawvid, window):
 
-    video = cv.VideoCapture(vidfile)
+    video = cv.VideoCapture(rawvid)
     ct = 0
     initial_frame = trajectory['frame'].to_numpy()[0]
     while ct < initial_frame:
@@ -13,13 +14,12 @@ def montage_generator(expid, trajectory, rawvid):
         assert exists, "Frame doesn't exist!"
         ct += 1
 
-    
-    for capture in trajectory.iterrows():
+    for _, capture in trajectory.iterrows():
         exists, img = video.read()
         assert exists, "Frame doesn't exist!"
         frame = capture['frame']
-        pos_x = capture['thorax_x']
-        pos_y = capture['thorax_y']
+        pos_x = int(capture['thorax_x'])
+        pos_y = int(capture['thorax_y'])
         roi = img[pos_y-window:pos_y+window, pos_x-window:pos_x+window]
         yield roi
 
