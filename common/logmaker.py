917fe17b01ebd6b2d6ff4de0215edef5bda4c691
maelstrom <cephalopodmaster@protonmail.com>
2022-11-01 14:48:12 -0400

    comments added

--- common/logmaker.py
+++ common/logmaker.py
@@ -2,6 +2,14 @@ import git
 import os
 
 
+# TODO: Work in progress; need to check that this actually works
+
+# Whenever the log generator is called, it will create a dictionary that
+# contains all the information needed to reproduce the output. Currently this
+# amounts to the list of source files used, and the git hash for the code that
+# was used (basically, to keep track of the code version information). Another
+# field should be added later to save the parameters.
+
 def log_generator(expids):
 
     repo = git.Repo(search_parent_directories=True)
