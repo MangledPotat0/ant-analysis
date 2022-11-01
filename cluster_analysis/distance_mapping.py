1b98cdcdec9cbf5abb69f903cd0a390cbed118c9
maelstrom <cephalopodmaster@protonmail.com>
2022-11-01 11:18:14 -0400

    Comments added

--- cluster_analysis/distance_mapping.py
+++ cluster_analysis/distance_mapping.py
@@ -1,27 +1,26 @@
-################################################################################
-#                                                                              #
-#   Created: 2022/06/12                                                        #
-#   Last modified: 2022/06/13                                                  #
-#                                                                              #
-################################################################################
-
 import argparse as arg
 import h5py
 import json
 import numpy as np
 import scipy.spatial as sp
 
+mlem = 'mlem'
 
+
+# Setting up paths
 with open('../paths.json','r') as f:
     paths = json.load(f)
     codepath = paths['codepath']
     datapath = paths['datapath']
 
-src = str(datapath + 'preprocessed' + '\\')
+src = str(datapath + 'preprocessed' + '/')
 
+
+# Load trajectory data from file, return numpy Array object
 
 def load_trajectories(fname):
-    with h5py.File('{}_proc.hdf5'.format(fname), 'r') as dfile:
+    with h5py.File('{}{}/{}_proc.hdf5'.format(srcpath,fname,fname),
+                                                        'r') as dfile:
         keys = dfile.keys()
         length = max(len(dfile[key]) for key in keys)
         trajectories = np.full((len(keys),length,4,2), 0, dtype=float)
@@ -33,6 +32,9 @@ def load_trajectories(fname):
     
     return trajectories
 
+
+# Find pairwise distance for every ants and return the distances as a square
+# matrix. Note that the diagonal elements should all be 0.
 
 def compute_distances(trajectories):
     n = len(trajectories)
@@ -53,11 +55,11 @@ if __name__ == '__main__':
     args = vars(ap.parse_args())
     expid = args['expid']
 
-    trajectories = load_trajectories("{}{}\\{}".format(src, expid, expid))
+    trajectories = load_trajectories("{}{}/{}".format(src, expid, expid))
 
     distances = compute_distances(trajectories)
 
-    with h5py.File('{}{}\\{}_distance_matrices.hdf5'.format(src, expid, expid),
+    with h5py.File('{}{}/{}_distance_matrices.hdf5'.format(src, expid, expid),
                    'w') as outfile:
         outfile.create_dataset('matrix', data = distances)
 
