78153c0934b9c2c0c7e765334c803b34397a0332
maelstrom <cephalopodmaster@protonmail.com>
2022-11-01 11:27:02 -0400

    Comments added

--- cluster_analysis/kmc.py
+++ cluster_analysis/kmc.py
@@ -6,25 +6,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
-from sklearn import cluster
+from sklearn import cluster.KMeans as model
 import seaborn as sns
 import seaborn_image as isns
 from yellowbrick.cluster import KElbowVisualizer
 
-
 # Source: https://www.reneshbedre.com/blog/kmeans-clustering-python.html
 
-# Settings
-model = cluster.KMeans()
 
+# Setting paths
 with open('../paths.json','r') as f:
     paths = json.load(f)
     codepath = paths['codepath']
-    datapath = os.path.dirname(str(paths['datapath']+
-                               '\\trajectories\\clustering\\a')) + '\\'
-    videopath = os.path.dirname(str(paths['datapath']+
-                                '\\videos\\clustering\\a'))+'\\'
+    datapath = paths['datapath']
 
+today = date.today()
+today = today.strftime('%Y%m%d')
+
+outputpath = str(datapath + 'processed/cluster_kmeans/')
+figspath = str(datapath + 'processed/cluster_kmeans/' + today + '/')
+
+# Attempt to create the directories
+try:
+    os.mkdir(outputpath)
+except OSError:
+    pass
+
+try:
+    os.mkdir(figspath)
+except OSError:
+    pass
+
+
+# Knee (Elbow) locator
+
+# Input:
+#  frame    | numpy Array object containing image
+#  frameset | pandas DataFrame object containing all ants in current frame
+#  t        | int for current frame number
+
+# Output:
+#  visualizer.elbow_value_ | float value indicating the position of the elbow
 
 def find_optimal_kvalue(frame, frameset, t):
 
@@ -39,6 +61,14 @@ def find_optimal_kvalue(frame, frameset, t):
     return visualizer.elbow_value_
 
 
+# Do the K-means clustering
+
+# Input:
+#  Same as find_optimal_kvalue()
+
+# Output:
+#  NONE (Labeled image is saved directly to figspath)
+
 def perform_kmeans(frame, frameset, t):
     kmc = cluster.KMeans(n_clusters=k, init='k-means++', n_init = 20,
                          algorithm='auto').fit(frameset)
@@ -49,10 +79,9 @@ def perform_kmeans(frame, frameset, t):
     sns.scatterplot(data=frameset, x='x', y='y', hue=kmc.labels_)
     sns.scatterplot(data=centers, x='x', y='y', marker='X', 
                     label='centroids')
-    plt.savefig('kmeans_vis{}.png'.format(t))
+    plt.savefig('{}kmeans_vis{}.png'.format(figspath,t))
     plt.close()
     
-
 
 
 if __name__ == '__main__':
@@ -67,11 +96,12 @@ if __name__ == '__main__':
     dfile = h5py.File(str(datapath+args['file']), 'r')
     vidstream = cv.VideoCapture(str(videopath+args['video']))
     maxclusters = len(dfile.keys())
-    maxframe = 12000
+    maxframe = int(vidstream.get(cv.CAP_PROP_FRAME_COUNT))
+    stride = 250
 
     for t in range(maxframe):
         _, frame = vidstream.read()
-        if t % 250 == 0:
+        if t % stride == 0:
             frameset = pd.DataFrame(columns=['x', 'y'])
             ct = 0
             for key in dfile.keys():
@@ -82,7 +112,8 @@ if __name__ == '__main__':
                                             columns=['x','y'])
                     frameset = frameset.append(position)
                 except IndexError:
-                    print('idk')
+                    print('Warning: IndexError. ',
+                          'Likely means a missing instance.')
                 ct += 1
             k = find_optimal_kvalue(frame, frameset, t)
             perform_kmeans(frame, frameset, t)
